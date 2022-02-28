44cbe430ddb487ddfe1fe06f74057e454c30f5e0
Fayaz <flalani100@gmail.com>
2022-02-27 19:43:57 -0500

    Added and tested update category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -61,6 +61,27 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+    )
+      .then(dbCategoryData => {
+        if (!dbCategoryData) {
+          res.status(404).json({message: 'No category found with this id'});
+          return;
+        }
+        res.json(dbCategoryData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   // update a category by its `id` value
 });
 
