5785e8a3d91550d650379086c7745243a0ab91d7
Fayaz <flalani100@gmail.com>
2022-02-27 21:17:02 -0500

    Added tag update method

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -68,6 +68,28 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
+  console.log('==============')
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+    )
+      .then(dbTagData => {
+        if (!dbTagData) {
+          res.status(404).json({message: 'No tag found with this id'});
+          return;
+        }
+        res.json(dbTagData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   // update a tag's name by its `id` value
 });
 
