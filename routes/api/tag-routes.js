0bc0523369c442aeb7cad6dccb466f3546c83b9c
Fayaz <flalani100@gmail.com>
2022-02-27 21:10:45 -0500

    Added find one tag method

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -24,7 +24,32 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+  console.log('==============')
+    Tag.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Product,
+          as:'tag_products',
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+      ]
+    })
+    .then(dbTagData => {
+      if(!dbTagData) {
+        res.status(404).json({message: 'No tag found with this id'})
+        return;
+      }
+      res.json(dbTagData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
   
+    console.log('id found')
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
